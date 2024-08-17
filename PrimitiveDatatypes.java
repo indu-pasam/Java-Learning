ea89efc70c4ffe1defa951f77062a15252cb05a5
indu-pasam <pasamindu1433@gmail.com>
2024-08-17 22:15:23 +0530

    Create PrimitiveDatatypes.java

---
+++ PrimitiveDatatypes.java
@@ -0,0 +1,20 @@
+class Primitive{
+    public static void main(String[] args) {
+        byte b=120;
+        short s = 300;
+        int i=4000;
+        long l=5000L;
+        float f=33.4f;
+        double d=456.1;
+        boolean bo=true;
+        System.out.println("b="+b);
+        System.out.println("s="+s);
+        System.out.println("i="+i);
+        System.out.println("l="+l);
+        System.out.println("f="+f);
+        System.out.println("d="+d);
+        System.out.println("bo="+bo);
+        System.out.print(Integer.MIN_VALUE +"to" );
+        System.out.println(Integer.MAX_VALUE);
+  }
+}
