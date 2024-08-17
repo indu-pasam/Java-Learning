66af1312825814e6b48416a001304bae6acefbce
indu-pasam <pasamindu1433@gmail.com>
2024-08-17 22:16:10 +0530

    Create Ranges.java

---
+++ Ranges.java
@@ -0,0 +1,25 @@
+class Ranges {
+    public static void main(String[] args) {
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Character.MAX_VALUE);
+        System.out.println(Character.MIN_VALUE);
+        System.out.println(Integer.SIZE/8+"Bytes");
+        System.out.println(Float.SIZE/8+"Bytes"); 
+        System.out.println(Double.SIZE/8+"Bytes");
+        System.out.println(Long.SIZE/8+"Bytes");
+        System.out.println(Short.SIZE/8+"Bytes");
+        System.out.println(Character.SIZE/8+"Bytes");
+        System.out.println(Byte.SIZE/8+"Byte");
+    }
+}
