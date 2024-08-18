ce170abfa395ccdc408d74cb4b9a009398c77742
indu-pasam <pasamindu1433@gmail.com>
2024-08-18 22:15:04 +0530

    Update Input.java

--- Input.java
+++ Input.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 class Input {
     public static void main(String[] args) {
+        System.out.println("enter a and b");
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
