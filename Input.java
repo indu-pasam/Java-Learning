68c7bb7d6fa71b9c9e02160e58e0cc15d54adc44
indu-pasam <pasamindu1433@gmail.com>
2024-08-18 21:52:28 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Input {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        float c=a+b;
+        System.out.println("result is"+c);
+        
+        sc.close();
+    }
+}
