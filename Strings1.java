2247a0bd530d374e161250f4b41e638731d0125b
indu-pasam <pasamindu1433@gmail.com>
2024-08-25 21:42:01 +0530

    Add files via upload

---
+++ Strings1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Strings1 {
+    public static void main(String[] args) {
+        String a;//it is stored string pool
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextLine();
+        System.out.println(a);
+        //System.out.println(a);
+        String b=new String("Welcome");//it is stored in heap memory
+        System.out.println(b);
+        System.out.println(a==b);//== used for comparing the addresses only
+        System.out.println(a=b);
+        sc.close();
+    }
+}
