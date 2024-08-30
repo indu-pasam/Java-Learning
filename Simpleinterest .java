d7f48fb4a5a14434298161a4647c2de2e99a0168
indu-pasam <pasamindu1433@gmail.com>
2024-08-30 22:10:48 +0530

    Create Simpleinterest .java

---
+++ Simpleinterest .java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class GFG {
+    public static void main(String args[])
+    {
+        // We can change values here for
+        // different inputs
+        float P = 10000, R = 5, T = 5;
+
+        // Calculate simple interest
+        float SI = (P * T * R) / 100;
+        System.out.println("Simple interest = " + SI);
+    }
+}
