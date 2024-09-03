896a1a6440fbf6837c55995e9f93bff77979c4b7
indu-pasam <pasamindu1433@gmail.com>
2024-09-03 20:21:31 +0530

    Update Swap2.java

--- Swap2.java
+++ Swap2.java
@@ -1,6 +1,6 @@
 public class Swap2 {
     public static void main(String[] args) {
-        int a=10;int b=3;//swapping without using temp var
+        int a=1;int b=3;//swapping without using temp var
         System.out.println("before swapping" + "a:"+a+" "+"b:"+" "+b);
         a=a-b;//7
         b=a+b;//7+3=10
