7762fa7f6ddb1a6e6a0387506e1de86cb94797fe
indu-pasam <pasamindu1433@gmail.com>
2024-09-20 21:33:27 +0530

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,11 @@
+public class Class {
+    String breed="German shepard";
+    int height=30;
+    int weight=36;
+    public static void main(String[] args) {
+        Class dog=new Class();
+        System.out.println(dog.breed);
+        System.out.println(dog.height);
+        System.out.println(dog.weight);
+    }
+}
