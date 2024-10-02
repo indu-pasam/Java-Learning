28b9a447b63533ece168b49e0a67af7c30873e29
indu-pasam <pasamindu1433@gmail.com>
2024-10-02 15:59:29 +0530

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,7 @@
+package mypackage;
+public class  Abc{
+    public int a=100;
+    public void hello(){
+        System.out.println("Hello Hii welcome");
+    }
+}
---
+++ Def.java
@@ -0,0 +1,8 @@
+package mypackage;
+
+public class Def {
+    public static void main(String[] args) {
+        Abc s=new Abc();
+        System.out.println(s.a);
+    }
+}
