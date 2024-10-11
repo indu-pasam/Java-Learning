f22dd3d8b8178c9fbd1902e81f97bad92ec3799c
indu-pasam <pasamindu1433@gmail.com>
2024-10-11 16:21:35 +0530

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,50 @@
+package interfaceconcept;
+interface Laptop {
+    int a=10;
+    public void copy();
+    public void paste();
+    public void cut();
+    public void keyboard();
+}
+class Lenovo implements Laptop{
+    public void copy(){
+        System.out.println("Lenovo copy method");
+    }
+    public void paste(){
+        System.out.println("Lenovo paste method");
+    }
+    public void cut(){
+        System.out.println("Lenovo cut method"); 
+    }
+    public void keyboard(){
+        System.out.println("Lenovo keyboard method");
+    }
+}
+ class Apple implements Laptop{
+    public void copy(){
+        System.out.println("Apple copy method");
+    }
+    public void paste(){
+        System.out.println("Apple paste method");
+    }
+    public void cut(){
+        System.out.println("Apple cut method");
+    }
+    public void keyboard(){
+        System.out.println("Apple keyboard method");
+    }
+}
+class User{
+    public static void main(String[] args) {
+    Lenovo a=new Lenovo();
+        a.copy();
+        a.paste();
+        a.keyboard();
+    Apple b=new Apple();
+        b.copy();
+        b.paste();
+        b.keyboard();
+        System.out.println(Laptop.a);//accessing interface variable
+    }
+}
+
