8bab3047f038ca6bedc8c77d63cd7208520d0874
indu-pasam <pasamindu1433@gmail.com>
2024-10-15 21:06:52 +0530

    Update Prime-num1ton.java

--- Prime-num1ton.java
+++ Prime-num1ton.java
@@ -1,44 +1,20 @@
-class gfg {
-
-    // Function to print all the
-    // prime numbers till N
-    static void prime_N(int N)
-    {
-        // Declaring the variables
-        int x, y, flg;
-
-        // Printing display message
-        System.out.println(
-            "All the Prime numbers within 1 and " + N
-            + " are:");
-
-        // Using for loop for traversing all
-        // the numbers from 1 to N
-        for (x = 2; x <= N; x++) {
-
-            // Using flag variable to check
-            // if x is prime or not
-            flg = 1;
-
-            for (y = 2; y * y <= x; y++) {
-                if (x % y == 0) {
-                    flg = 0;
+public class Prime1ton {
+    void checkprime(int n){
+        for(int i=1;i<=n;i++){
+            int count=0;
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0){
+                    count++;
                     break;
                 }
             }
-
-            // If flag is 1 then x is prime but
-            // if flag is 0 then x is not prime
-            if (flg == 1)
-                System.out.print(x + " ");
+            if(count==0&&i!=1){
+                System.out.println(i);
+            }
         }
     }
-
-    // The Driver code
-    public static void main(String[] args)
-    {
-        int N = 45;
-
-        prime_N(N);
+    public static void main(String[] args) {
+        Prime1ton a=new Prime1ton();
+        a.checkprime(20);
     }
 }
